be1d1d80905d0be96cffbd11ea06b900baba5e57
Gabriel Mateus de Andrade <saintseiyatcl@gmail.com>
2024-11-04 13:37:26 -0300

    WIP

--- my-app/src/lib/axios.ts
+++ my-app/src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL,
-    
-})
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || process.env.NEXTAUTH_URL,
+});
